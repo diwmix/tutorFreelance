6985a0d68fcd92dd6ef4ef81ed20cdac6b97c327
diwmix <sanuaburdun15@gmail.com>
2023-11-08 18:04:42 +0200

    first commit

--- main.py
+++ main.py
@@ -1,46 +1,26 @@
-letter_to_symbol = {
-    'a': '!',
-    'b': '"',
-    'c': '#',
-    'd': '$',
-    'e': '%',
-    'f': '&',
-    'g': "'",
-    'h': '(',
-    'i': ')',
-    'j': "*",
-    'k': '+',
-    'l': ',',
-    'm': '-',
-    'n': '.',
-    'o': '/',
-    'p': ':',
-    'q': ';',
-    'r': '<',
-    's': '=',
-    't': '>',
-    'u': '?',
-    'v': '@',
-    'w': "[",
-    'x': '\\',
-    'y': ']',
-    'z': '^'
-}
+def swap_zeros_with_first_non_zero(num):
+    num_str = str(num)
 
-# Функция для замены букв на символы
-def replace_letters(input_text):
-    result = ''
-    for letter in input_text:
-        # Если буква есть в словаре, заменяем её символом, в противном случае оставляем как есть
-        result += letter_to_symbol.get(letter, letter)
-    return result
+    # Находим индекс первой ненулевой цифры
+    first_non_zero_index = next((i for i, digit in enumerate(num_str) if digit != '0'), None)
 
-# Получаем ввод пользователя
-user_input = input("Введите текст, чтобы заменить буквы на символы: ")
+    if first_non_zero_index is not None and first_non_zero_index < len(num_str):
+        # Создаем список символов для измененной строки
+        new_str = list(num_str)
 
-# Заменяем буквы на символы
-output_text = replace_letters(user_input)
+        # Меняем местами нули с первой ненулевой цифрой
+        new_str[first_non_zero_index], new_str[0] = new_str[0], new_str[first_non_zero_index]
 
-# Выводим результат
-print("Результат замены:")
-print(output_text)
+        # Преобразуем список обратно в строку
+        result_str = ''.join(new_str)
+
+        result = int(result_str)
+        return result
+    else:
+        # Если нет ненулевых цифр, возвращаем исходное число
+        return num
+
+# Пример использования
+number = 4820901
+result = swap_zeros_with_first_non_zero(number)
+print(result)
